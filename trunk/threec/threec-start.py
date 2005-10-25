de74142e488c29fad3957a90f41a753faba20c45
rrenaud <rrenaud@0f8c8f77-c002-0410-8ea5-8f3f639e2aaf>
2005-10-25 03:14:11 +0000

    useless comment to check that the mailing svn change mailing list is working

--- trunk/threec/threec-start.py
+++ trunk/threec/threec-start.py
@@ -8,6 +8,8 @@ from os.path import *
 # look for setup.py in this directory. If it's not there, this script is
 # probably installed
 if exists(join(dirname(__file__), "setup.py")):
+    # change this so that we can get around hard-coding dan's absolute
+    # DB path
     cherrypy.config.update(file="dev.cfg")
 else:
     cherrypy.config.update(file="prod.cfg")
